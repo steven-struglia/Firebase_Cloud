1bb5758c6365044c5f87a982fe0e8aae65e43b00
Steven Struglia <40417554+steven-struglia@users.noreply.github.com>
2019-05-26 22:59:14 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 //Firestore imports
-const cron = require("node-schedule");
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
+const cron = require('node-schedule');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 const SENDGRID_API_KEY = functions.config().sendgrid.key;
 
-const sgMail = require("@sendgrid/mail");
+const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 var data = admin.firestore()
 
 //sends an email for each new notification added to a users notification document
 exports.newNoti = functions.firestore
-  .document("/users/{userID}/notifications/{notificationID}")
-  .onCreate((event, context) => {
+  .document('/users/{userID}/notifications/{notificationID}')
+  .onCreate( (context, event) => {
     const notificationID = event.params.notificationID;
     const user = event.params.userID;
     const fsdb = admin.firestore();
-    return fsdb.collection("notifications")
+    return fsdb.collection('notifications')
       .doc(notificationID)
       .get()
       .then(doc => {
         const noti= doc.data()
         const msg = {
           to: user.email,
-          from: "//WHATEVER EMAIL WE ARE USING",
+          from: 'noreply@insider.works',
           subject: noti.message,
           //custom template
           templateID: 'd-faf87202a4a64712933c5beb20c4021d',
@@ -37,13 +37,13 @@ exports.newNoti = functions.firestore
         return sgMail.send(msg);
       })
       .then(() => console.log('Email sent!'))
-      .catch(err => console.log(err))
+      .catch(err => console.log('Error: ', err))
   });
 
 //final daily solution with included ticker field
 exports.daily = functions.pubsub.schedule('5 11 * * *').onRun((event, context) => {
-  db.collection(`/users/{userID}/notifications/{notificationID}`)
-    .where("read", "==", "false").where("ticker", "==", "daily")
+  db.collection('/users/{userID}/notifications/{notificationID}')
+    .where('read', '==', 'false').where('ticker', '==', 'daily')
     .get()  
     .then(function(querySnapshot) {
         querySnapshot.forEach(function(documentSnapshot) {
@@ -53,7 +53,7 @@ exports.daily = functions.pubsub.schedule('5 11 * * *').onRun((event, context) =
           var e = userID.email;
           const msg = {
             to: e,
-            from: "//WHATEVER EMAIL WE ARE USING",
+            from: 'noreply@insider.works',
             subject: data.message,
             //custom template
             templateID: 'd-faf87202a4a64712933c5beb20c4021d',
@@ -67,13 +67,13 @@ exports.daily = functions.pubsub.schedule('5 11 * * *').onRun((event, context) =
         })
     }
     .catch(function(error) {
-      console.log("Error sending daily email: ", error)
+      console.log('Error sending daily email: ', error)
     }));
   });
 //final weekly solution with included ticker field
 exports.weekly = functions.pubsub.schedule('* * * * 1').onRun((event, context) => {
-  db.collection(`/users/{userID}/notifications/{notificationID}`)
-    .where("read", "==", "false").where("ticker", "==", "daily")
+  db.collection('/users/{userID}/notifications/{notificationID}')
+    .where('read', '==', 'false').where('ticker', '==', 'daily')
     .get()  
     .then(function(querySnapshot) {
         querySnapshot.forEach(function(documentSnapshot) {
@@ -83,7 +83,7 @@ exports.weekly = functions.pubsub.schedule('* * * * 1').onRun((event, context) =
           var e = userID.email;
           const msg = {
             to: e,
-            from: "//WHATEVER EMAIL WE ARE USING",
+            from: '//noreply@insider.works',
             subject: data.message,
             //custom template
             templateID: 'd-faf87202a4a64712933c5beb20c4021d',
@@ -97,6 +97,6 @@ exports.weekly = functions.pubsub.schedule('* * * * 1').onRun((event, context) =
         })
     }
     .catch(function(error) {
-      console.log("Error sending daily email: ", error)
+      console.log('Error sending daily email: ', error)
     }));
   });
