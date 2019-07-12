d9c073ed8781c774106d8ebbce9292cd89d5e0df
Steven Struglia <40417554+steven-struglia@users.noreply.github.com>
2019-07-12 09:57:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ const mailTransport = nodemailer.createTransport({
   }
 });
 
-// Your company name to include in the emails
-// TODO: Change this to your app or company name to customize the email sent.
+// Company name to include in the emails
 const APP_NAME = "Insider WORKS";
 
 // [START sendWelcomeEmail]
